2fb95b8bf4eecc1affcde52e10f84f8a074626a4
HerbertHe <Herbert.He0229@gmail.com>
2021-05-15 20:05:10 +0800

    chore :wrench: : support vite  proxy for development

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import Home from "./pages/Home"
 
-const App = () => (
-    <Router>
-        <Route path="/" exact>
-            <Home />
-        </Route>
-    </Router>
-)
+const App = () => {
+    return (
+        <Router>
+            <Route path="/" exact>
+                <Home />
+            </Route>
+        </Router>
+    )
+}
 
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import reactRefresh from "@vitejs/plugin-react-refresh"
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [reactRefresh()],
+    server: {
+        proxy: {
+            "^/api/.*": {
+                target: "http://127.0.0.1:8080",
+                changeOrigin: true,
+            },
+        },
+    },
 })
