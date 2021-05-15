47f2ddee1cff14d14beaf7e467ff52459785e29e
HerbertHe <Herbert.He0229@gmail.com>
2021-05-15 20:47:14 +0800

    refactor :recycle: : update cli tool

--- bin/index.js
+++ bin/index.js
@@ -1,19 +1,37 @@
 #!/usr/bin/env node
 const program = require("commander")
 const path = require("path")
+const fs = require("fs")
 const { exec } = require("child_process")
 
-const { readConfigFile } = require("../utils/utils")
+const { readConfigFile, updateConfigFile } = require("../utils/utils")
 const { writeNginxConfig, copyConfigFile2Nginx } = require("./nginx")
+const { usablePort } = require("./utils/network")
+const { logAdviceString } = require("./utils")
 
-program.name("logm").version("0.0.1")
+const { version } = JSON.parse(
+    fs.readFileSync(path.resolve(__dirname, "../package.json"), {
+        encoding: "utf-8",
+    })
+)
+
+program.name("logm").version(version)
 
 program
     .command("run")
     .description("启动项目")
-    .action(() => {
+    .action(async () => {
         const { Port } = readConfigFile()
-        process.env.LOGM_PORT = Port
+        const port = await usablePort(Port)
+
+        // 更新不正确的端口配置
+        if (port !== Port) {
+            updateConfigFile({ key: "Port", value: port })
+        }
+
+        process.env.LOGM_PORT = port
+        console.log(logAdviceString(`服务端运行: http://127.0.0.1:${port}`))
+
         exec(
             `node ${path.resolve(__dirname, "../index.js")}`,
             (err, stdout, stderr) => {
--- utils/utils.js
+++ utils/utils.js
@@ -20,6 +20,45 @@ const readConfigFile = () => {
     return JSON.parse(config)
 }
 
+/**
+ * 更新配置文件
+ */
+const updateConfigFile = ({ key, value }) => {
+    const cwd = process.cwd()
+    const files = fs.readFileSync(cwd)
+    let config = null
+    if (files.includes(".logmrc.json")) {
+        config = JSON.parse(
+            fs.readFileSync(path.resolve(cwd, ".logmrc.json"), {
+                encoding: "utf-8",
+            })
+        )
+        config[key] = value
+        fs.writeFileSync(
+            path.resolve(cwd, ".logmrc.json"),
+            JSON.stringify(config),
+            {
+                encoding: "utf-8",
+            }
+        )
+    } else {
+        config = JSON.parse(
+            fs.readFileSync(path.resolve(__dirname, "../.logmrc.json"), {
+                encoding: "utf-8",
+            })
+        )
+        config[key] = value
+        fs.writeFileSync(
+            path.resolve(__dirname, ".logmrc.json"),
+            JSON.stringify(config),
+            {
+                encoding: "utf-8",
+            }
+        )
+    }
+}
+
 module.exports = {
     readConfigFile,
+    updateConfigFile,
 }
